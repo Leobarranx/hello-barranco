46d6bbf82fa8a84294c47fdf19e8d6a518bed04f
Leobarranx <leonardo.barranco@capellinisauro.edu.it>
2025-04-24 07:30:30 +0000

    stampo tre volte
    git push -u origin main
    
    git logu
    git log
    clear

--- hello.java
+++ hello.java
@@ -4,4 +4,9 @@ public class hello {
     public static void main(String [] args) {
         System.out.println("hello barranco");
     }
+
+    //stampa tre volte il nome
+    for(int i=0; i<3; i++){
+        System.out.println("barranco");
+    }
 }
